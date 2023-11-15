500c305913598960944bb80ba1e9753f52e24d5e
Jus Jae <42322548+JusJae@users.noreply.github.com>
2023-11-15 23:16:27 +0000

    build(add pymongo and mongo config to app.py):

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ['IP'] = '0.0.0.0'
+os.environ['PORT'] = '5000'
+os.environ['SECRET_KEY'] = '3aDyrLxxuesUmt3EO2CcQ7nrKrP0FDoE'
+os.environ['MONGO_URI'] = 'mongodb+srv://jusjaedev:ThK2DMdAWMs2Wqj9@productlog.sxpjski.mongodb.net/?retryWrites=true&w=majority'
+os.environ['MONGO_DBNAME'] = 'productlog'
